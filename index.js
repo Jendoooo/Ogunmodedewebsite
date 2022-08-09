a4643252bf8a4e0e2b6ca6c9f1f2c7569f2b1afa
Olajide Ayeola <olajideayeola@gmail.com>
2022-08-09 15:50:06 +0100

    After addition of simple script and media query for dropdown menu for smaller screens

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var viewhome = document.getElementById("navLinks")
+
+function showMenu(){
+                    
+    viewhome.style.right ="0"
+
+}
+function hideMenu(){
+    viewhome.style.right ="-200px"
+}
+
+
+
+var testing 
+console.log("I no sure this guy dey work ")
